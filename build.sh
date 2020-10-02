33c802c01f8fff236d58bfb3cfb17ed5f5f589c8
JiajunLin <ljj@gmail.com>
2020-10-02 11:14:12 -0500

    fix docker

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ sudo docker stop angularbackend
 sudo docker rm angularbackend
 sudo docker rmi angularbackendimage
 sudo docker build -t angularbackendimage .
-sudo docker run -d -p 3000:3000 --name angularbackenddemo angularbackendimage
+sudo docker run -d -p 3000:3000 --name angularbackend angularbackendimage
